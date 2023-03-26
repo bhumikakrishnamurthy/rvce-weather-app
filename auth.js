32024320aa137be9fc3e655fb2bc0f77dc340ac9
rnitin <rnitin@vmware.com>
2023-03-27 03:25:45 +0530

    working code

--- auth.js
+++ auth.js
@@ -1,13 +1,28 @@
 function authenticateUser() {
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-
-  // Check if username and password match hardcoded values
-  if (email === "myusername@gmail.com" && password === "mypassword") {
-    // Redirect user to dashboard page
-    window.location.href = "dashboard.html";
-  } else {
-    // Show error message
-    document.getElementById("error-message").innerHTML = "Invalid email or password";
-  }
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const loginUrl = "http://localhost:8080/login";
+  const loginData = { email: email, password: password };
+  const loginRequestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginData),
+  };
+  fetch(loginUrl, loginRequestOptions)
+      .then(response => response.text())
+      .then(status => {
+          if (status === false) {
+              const errorMessage = document.getElementById("error-message");
+              errorMessage.innerHTML = error.message;
+              window.location.href = "/login.html";
+          } else {
+              window.location.href = "/dashboard.html";
+          }
+      })
+      .catch((error) => {
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.innerHTML = error.message;
+      });
 }
